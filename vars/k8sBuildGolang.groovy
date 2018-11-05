dbbf9fc4d6d18838b20a1f26848bcaace16a3421
Miguel David <migueldavid@gmail.com>
2018-11-05 18:46:44 +0000

    Update k8sBuildGolang.groovy

--- vars/k8sBuildGolang.groovy
+++ vars/k8sBuildGolang.groovy
@@ -1,5 +1,5 @@
 def call(String programToBuild) {
     sh "go get -d -v -t"
     //sh "go test --cover -v ./... --run UnitTest"
-    sh "go build -v -o ${programToBuild}"
+    sh "go build -v ${programToBuild}"
 }
