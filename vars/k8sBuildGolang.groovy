0de1ed27293b4b8547b8f4086834b99b842c4377
Miguel David <migueldavid@gmail.com>
2018-11-13 17:08:30 +0000

    changing name of var to sourceFile

--- vars/k8sBuildGolang.groovy
+++ vars/k8sBuildGolang.groovy
@@ -1,5 +1,5 @@
-def call(binary) {
+def call(sourceFile) {
     sh "go get -d -v -t"
     sh "go test --cover -v ./... --run UnitTest"
-    sh "go build -v -o ${binary}"
+    sh "go build -v -o ${sourceFile}"
 }
